3faad53e983ba8bb57dd68f80a5e78435ddd7d31
mfin23 <mfindlay@scu.edu>
2018-06-10 18:28:44 -0700

    Seq2seq hashes now

--- seq2seq.py
+++ seq2seq.py
@@ -107,74 +107,19 @@ if __name__ == "__main__":
     data_path = 'fra-eng/fra.txt'
 
     # Vectorize the data.
-    input_texts = []
-    target_texts = []
-    input_characters = set()
-    target_characters = set()
 
-    for doc in utils.DataUtils.X_train[:1000]:
-        for words in doc:
-            try:
-                w = words.decode('utf-8')
-                input_text = w
-                target_text = w
-                # We use "tab" as the "start sequence" character
-                # for the targets, and "\n" as "end sequence" character.
-                target_text = '\t' + target_text + '\n'
-                input_texts.append(input_text)
-                target_texts.append(target_text)
-                for char in input_text:
-                    if char not in input_characters:
-                        input_characters.add(char)
-                for char in target_text:
-                    if char not in target_characters:
-                        target_characters.add(char)
-            except UnicodeDecodeError:
-                pass
-
-    input_characters = sorted(list(input_characters))
-    target_characters = sorted(list(target_characters))
-    num_encoder_tokens = len(input_characters)
-    num_decoder_tokens = len(target_characters)
-    max_encoder_seq_length = max([len(txt) for txt in input_texts])
-    max_decoder_seq_length = max([len(txt) for txt in target_texts])
-
-    print('Number of samples:', len(input_texts))
-    print('Number of unique input tokens:', num_encoder_tokens)
-    print('Number of unique output tokens:', num_decoder_tokens)
-    print('Max sequence length for inputs:', max_encoder_seq_length)
-    print('Max sequence length for outputs:', max_decoder_seq_length)
-
-    input_token_index = dict(
-                         [(char, i) for i, char in enumerate(input_characters)])
-    target_token_index = dict(
-                          [(char, i) for i, char in enumerate(target_characters)])
-
-    encoder_input_data = np.zeros(
-                              (len(input_texts), max_encoder_seq_length, num_encoder_tokens),
-                              dtype='float32')
-    decoder_input_data = np.zeros(
-                              (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
-                              dtype='float32')
-    decoder_target_data = np.zeros(
-                               (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
-                               dtype='float32')
-
-    for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
-        for t, char in enumerate(input_text):
-            encoder_input_data[i, t, input_token_index[char]] = 1.
-        for t, char in enumerate(target_text):
-            # decoder_target_data is ahead of decoder_input_data by one timestep
-            decoder_input_data[i, t, target_token_index[char]] = 1.
-            if t > 0:
-                # decoder_target_data will be ahead by one timestep
-                # and will not include the start character.
-                decoder_target_data[i, t - 1, target_token_index[char]] = 1.
-
+    encoder_input_data, decoder_input_data, decoder_target_data, num_encoder_tokens, num_decoder_tokens = utils.DataUtils.prep_train_data(0, 1000)
     seq_model = seq2seq(latent_dim, num_encoder_tokens, num_decoder_tokens)
     seq_model.train(encoder_input_data, decoder_input_data, decoder_target_data)
 
-    for seq_index in range(100):
-        input_seq = encoder_input_data[seq_index: seq_index + 1]
-        states_value = seq_model.hash(input_seq)
-        print("Hash value: \n", states_value)
+    #enc_test, _, _, _, _= utils.DataUtils.prep_train_data(1000, 1100)
+    with open("hash_values.txt", "w") as f:
+        for seq_index in range(100):
+            input_seq = encoder_input_data[seq_index: seq_index + 1]
+            states_value = np.array(seq_model.hash(input_seq))
+            hash_code = utils.hash(states_value)
+            print("Hash value: \n", hash_code)
+            f.write(''.join(str(hash_code)))
+            f.write(',')
+            f.write(str(utils.DataUtils.Y_train[seq_index]))
+            f.write('\n')
--- utils.py
+++ utils.py
@@ -2,20 +2,9 @@ import os
 import scipy
 import tqdm
 import numpy as np
-"""
-def getSession(gpu_num, gpu_fraction):
-    os.environ["CUDA_VISIBLE_DEVICES"] = gpu_num
 
-    num_threads = os.environ.get("OMP_NUM_THREADS")
-    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_fraction)
-
-    if num_threads:
-        return tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, intra_op_parallelism_threads=num_threads))
-    else:
-        return tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)
-"""
 def hash(states):
-    state_string = states[0]+states[1]
+    state_string = states.flatten()
     return [1 if i >0 else 0 for i in state_string]
 
 class DataUtils(object):
@@ -70,3 +59,70 @@ class DataUtils(object):
                     print("Word not in vocab, skipping")
 
         DataUtils.word_vec_train.append(doc_vec)
+
+    @staticmethod
+    def prep_train_data(start_ind=0, end_ind=100):
+        input_texts = []
+        target_texts = []
+        input_characters = set()
+        target_characters = set()
+        for doc in DataUtils.X_train[start_ind:end_ind]:
+            for words in doc:
+                try:
+                    w = words.decode('utf-8')
+                    input_text = w
+                    target_text = w
+                    # We use "tab" as the "start sequence" character
+                    # for the targets, and "\n" as "end sequence" character.
+                    target_text = '\t' + target_text + '\n'
+                    input_texts.append(input_text)
+                    target_texts.append(target_text)
+                    for char in input_text:
+                        if char not in input_characters:
+                            input_characters.add(char)
+                    for char in target_text:
+                        if char not in target_characters:
+                            target_characters.add(char)
+                except UnicodeDecodeError:
+                    pass
+
+        input_characters = sorted(list(input_characters))
+        target_characters = sorted(list(target_characters))
+        num_encoder_tokens = len(input_characters)
+        num_decoder_tokens = len(target_characters)
+        max_encoder_seq_length = max([len(txt) for txt in input_texts])
+        max_decoder_seq_length = max([len(txt) for txt in target_texts])
+
+        print('Number of samples:', len(input_texts))
+        print('Number of unique input tokens:', num_encoder_tokens)
+        print('Number of unique output tokens:', num_decoder_tokens)
+        print('Max sequence length for inputs:', max_encoder_seq_length)
+        print('Max sequence length for outputs:', max_decoder_seq_length)
+
+        input_token_index = dict(
+                             [(char, i) for i, char in enumerate(input_characters)])
+        target_token_index = dict(
+                              [(char, i) for i, char in enumerate(target_characters)])
+
+        encoder_input_data = np.zeros(
+                                  (len(input_texts), max_encoder_seq_length, num_encoder_tokens),
+                                  dtype='float32')
+        decoder_input_data = np.zeros(
+                                  (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
+                                  dtype='float32')
+        decoder_target_data = np.zeros(
+                                   (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
+                                   dtype='float32')
+
+        for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
+            for t, char in enumerate(input_text):
+                encoder_input_data[i, t, input_token_index[char]] = 1.
+            for t, char in enumerate(target_text):
+                # decoder_target_data is ahead of decoder_input_data by one timestep
+                decoder_input_data[i, t, target_token_index[char]] = 1.
+                if t > 0:
+                    # decoder_target_data will be ahead by one timestep
+                    # and will not include the start character.
+                    decoder_target_data[i, t - 1, target_token_index[char]] = 1.
+
+        return encoder_input_data, decoder_input_data, decoder_target_data, num_encoder_tokens, num_decoder_tokens
